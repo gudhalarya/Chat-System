b77a2d8e1b7dcfb881d4783b18e93c78c679d2ce
gudhalarya <gudhalarya@gmail.com>
2025-12-14 23:44:38 +0530

    The first proper logic is here:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::{Duration, Instant};
 
-use actix::Actor;
+use actix::{Actor, StreamHandler};
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, web};
 use actix_web_actors::ws;
 
 struct WsConn{
@@ -30,4 +31,47 @@ impl WsConn{
             ctx.ping(b"ping");
         });
     }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsConn {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+
+            Ok(ws::Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
+
+            Ok(ws::Message::Text(text)) => {
+                println!("Received: {}", text);
+                ctx.text(format!("Echo: {}", text));
+            }
+
+            Ok(ws::Message::Close(reason)) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+
+            _ => {}
+        }
+    }
+}
+
+
+async fn ws_route(req:HttpRequest,stream:web::Payload)->Result<HttpResponse,Error>{
+    ws::start(WsConn{hb:Instant::now()}, &req, stream)
+}
+
+#[actix_web::main]
+async fn main ()->std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+        .route("/ws", web::get().to(ws_route))
+    })
+    .bind(("127.0.0.1",8080))?
+    .run()
+    .await
 }
