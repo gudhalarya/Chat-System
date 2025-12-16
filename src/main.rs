7876c4162f79f843ee756b51e8fa71a93c7db79d
gudhalarya <gudhalarya@gmail.com>
2025-12-16 23:43:46 +0530

    Fixed some issues

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,35 @@
+use futures_util::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
-use futures_util::{StreamExt, SinkExt};
 
+//this is where the main program starts 
+const PORT :&str = "127.0.0.1:8080";
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080")
-        .await
-        .expect("Failed to bind");
+async fn main (){
+    let listener = TcpListener::bind(PORT).await.expect("Could not find the port");
+    println!("Listener started on : {}",PORT);
+    while let Ok((stream,addr)) = listener.accept().await {
+        println!("Connection started at : {}",addr);
 
-    println!("🚀 WebSocket server running on ws://127.0.0.1:8080");
-
-    while let Ok((stream, addr)) = listener.accept().await {
-        println!("New connection from {}", addr);
-
-        tokio::spawn(async move {
+        tokio::spawn(async move{
             let ws_stream = match accept_async(stream).await {
-                Ok(ws) => ws,
-                Err(e) => {
-                    eprintln!("WebSocket error: {}", e);
+                Ok(ws)=>ws,
+                Err(e)=>{
+                    eprintln!("Websocket error : {}",e.to_string());
                     return;
                 }
             };
-
-            let (mut write, mut read) = ws_stream.split();
-
-            while let Some(msg) = read.next().await {
+            let (mut write,mut read)= ws_stream.split();
+            while let Some(msg) =read.next().await  {
                 let msg = match msg {
-                    Ok(m) => m,
-                    Err(_) => break,
+                    Ok(m)=>m,
+                    Err(_)=>break,
                 };
-
-                if msg.is_text() {
-                    
+                if msg.is_text(){
                     write.send(msg).await.unwrap();
                 }
             }
-
-            println!("Connection closed: {}", addr);
+            println!("Connection closed : {}",addr);
         });
     }
 }
